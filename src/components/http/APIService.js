5ac6ecbcce843147221cd1ef105d0db7486f268c
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-10-09 10:33:50 -0500

    Update APIService.js

--- src/components/http/APIService.js
+++ src/components/http/APIService.js
@@ -1,15 +1,51 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import axios from 'axios';
+const API_URL = 'http://127.0.0.1:8000/'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'https://movies-groyce.pythonanywhere.com/'*/
 
-Vue.use(Router)
+export class APIService {
+  constructor() {
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+  }
+
+  getMovie(param_pk) {
+    const url = `${API_URL}/api/movies/${param_pk}`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::"+jwtToken);
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.get(url, {headers: {Authorization: `jwt ${jwtToken}`}});
+  }
+
+  getMovieList() {
+    const url = `${API_URL}/api/movies/`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::"+jwtToken);
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.get(url, {headers: headers});
+
+  }
+
+  addNewMovie(movie){
+    const url = `${API_URL}/api/movies/`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.post(url, movie, {headers: headers});
+  }
+
+  updateMovie(movie){
+    const url = `${API_URL}/api/movies/${movie.pk}`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.put(url, movie, {headers: headers});
+  }
+
+  deleteMovie(movie_Pk){
+    const url = `${API_URL}/api/movies/${movie_Pk}`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.delete(url, {headers: headers});
+  }
+
+  authenticateLogin(credentials) {
+    const url = `${API_URL}/auth/`;
+    return axios.post(url, credentials);
+  }
+}
